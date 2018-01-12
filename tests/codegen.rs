85711b624b968e5fcf01ab9d3aba3640042dab93
Eliza Weisman <eliza@buoyant.io>
2018-01-12 11:08:18 -0800

    Add test for more complex case
    This should ensure we don't make imports like `use bar::{Bar, baz, quux::quuux};`, which are invalid.
    
    Signed-off-by: Eliza Weisman <eliza@buoyant.io>

--- tests/codegen.rs
+++ tests/codegen.rs
@@ -258,18 +258,22 @@ fn scoped_imports() {
     scope.new_module("foo")
         .import("bar", "Bar")
         .import("bar", "baz::Baz")
+        .import("bar::quux", "quuux::Quuuux")
         .new_struct("Foo")
         .field("bar", "Bar")
         .field("baz", "baz::Baz")
+        .field("quuuux", "quuux::Quuuux")
         ;
 
     let expect = r#"
 mod foo {
     use bar::{Bar, baz};
+    use bar::quux::quuux;
 
     struct Foo {
         bar: Bar,
         baz: baz::Baz,
+        quuuux: quuux::Quuuux,
     }
 }"#;
 
