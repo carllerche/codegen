580f527cb4ea43ad7ad0d931c6a34f1a4d4b5183
Travis CI User <travis@example.org>
2020-06-01 20:49:12 +0000

    Documentation for carllerche/codegen@899501d

---
+++ implementors/core/fmt/trait.Write.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["codegen"] = [{"text":"impl&lt;'a&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.Write.html\" title=\"trait core::fmt::Write\">Write</a> for <a class=\"struct\" href=\"codegen/struct.Formatter.html\" title=\"struct codegen::Formatter\">Formatter</a>&lt;'a&gt;","synthetic":false,"types":["codegen::formatter::Formatter"]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
---
+++ source-files.js
@@ -0,0 +1,3 @@
+var N = null;var sourcesIndex = {};
+sourcesIndex["codegen"] = {"name":"","files":["associated_type.rs","block.rs","body.rs","bound.rs","docs.rs","enum.rs","field.rs","fields.rs","formatter.rs","function.rs","impl.rs","import.rs","item.rs","lib.rs","module.rs","scope.rs","struct.rs","trait.rs","type.rs","type_def.rs","variant.rs"]};
+createSourceSidebar();
